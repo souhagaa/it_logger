bb09952f67c8ddbb800feb603b118a54ecc1637e
souhagaa <souha.gaaloul@hotmail.com>
2021-08-11 12:12:31 +0100

    Materialize setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  useEffect(() => {
+    // Initialize Materialize JS so we can use modals
+    M.AutoInit();
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
